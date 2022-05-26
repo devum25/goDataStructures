f9566874d623b5e095e693c0a442179d92e0ef34
dsrivast <devum.srivastava@gmail.com>
2022-05-26 16:32:01 +0530

    rehashing implementation

--- ds/hashtable.go
+++ ds/hashtable.go
@@ -1,16 +1,29 @@
 package ds
 
+import (
+	"fmt"
+	"strconv"
+)
+
 
 const ArraySize = 7
+var count =0
 
 //Hashtable structure
 // Hashtable will hold an array of type bucket
 type HashTable struct{
-	array [ArraySize]*bucket
+	array []*bucket
 }
 
-func NewHashTable() *HashTable{
-        h := &HashTable{}
+func NewHashTable(size int) *HashTable{
+	length := 0
+	if size == 0{
+            length = ArraySize
+	}else{
+		length = size
+	}
+	   arr := make([]*bucket,length)
+        h := &HashTable{array: arr}
 		for i:=0;i<len(h.array);i++{
 			h.array[i] = &bucket{}
 		}
@@ -31,20 +44,26 @@ type bucketNode struct{
 }
 //Insert
 func (h *HashTable) Insert(key string,val interface{}){
-	index := hash(key)
-	h.array[index].insert(key,val)
+	index := h.hash(key)
+	h.array[index].insert(key,val)	
+	count++
+	load_factor := float32(count)/float32(len(h.array))
+	if  load_factor > 0.7{
+		h.rehash()
+	}
+
 }
 
 //Search
 func (h *HashTable) Search(key string) interface{}{
-	index := hash(key)
+	index := h.hash(key)
 	return h.array[index].search(key)
 	// return nil
 }
 
 //Delete
 func (h *HashTable) Delete(key string){
-	 index := hash(key)
+	 index := h.hash(key)
 	 h.array[index].delete(key)
 }
 
@@ -53,6 +72,42 @@ func (b *bucket) insert(k string,v interface{}){
 	newNode := &bucketNode{key: k,val: v}
 	newNode.next = b.head
 	b.head = newNode
+}
+
+func (h *HashTable) rehash(){
+    // save the ptr to oldTable and we will do insertion in new table
+	oldTable := *h
+
+	count = 0
+
+	// increase the table size 
+	*h = *NewHashTable(2*ArraySize+1)
+    
+	// copy elements from old table to new table
+
+	for _,v := range oldTable.array{
+        currentNode := v.head
+		for currentNode != nil{
+			key := currentNode.key
+            val := currentNode.val
+			h.Insert(key,val)
+
+			currentNode = currentNode.next
+		}
+	}
+
+}
+
+func (h *HashTable) Print(){
+	for i,v := range h.array{
+		currentNode := v.head
+		fmt.Println()
+		fmt.Print(strconv.Itoa(i))
+		for currentNode != nil{
+            fmt.Print("->"+currentNode.key)
+			currentNode = currentNode.next
+		}
+	}
 }
 // bucket search
 func (b *bucket) search(k string) interface{}{
@@ -73,7 +128,7 @@ func (b *bucket) delete(k string){
 		b.head = b.head.next
 		return
 	}
-	
+
 	previousNode := b.head
 
 	for previousNode.next != nil{
@@ -87,11 +142,11 @@ func (b *bucket) delete(k string){
 }
 
 // hash function
-func hash(key string) int{
+func (h *HashTable) hash(key string) int{
 	sum := 0
 	for _,v := range key{
          sum += int(v)
 	}
 
-	return (sum%ArraySize)
+	return (sum%len(h.array))
 }
--- main.go
+++ main.go
@@ -1,15 +1,20 @@
 package main
 
-import (
-	"fmt"
-
-	"github.com/dev/datastructures/ds"
-)
+import "github.com/dev/datastructures/ds"
 
 func main(){
-      table := ds.NewHashTable()
+      table := ds.NewHashTable(0)
 
-      table.Insert("Devum","Developer")
-      table.Delete("Devum")
-      fmt.Println(table.Search("Devum"))
+      table.Insert("James","Developer")
+      table.Insert("Dany","Tester")
+      table.Insert("Andrew","Tester")
+      table.Insert("Michael","Tester")
+      table.Insert("Aryansh","Tester")
+      table.Insert("Nicolas","Tester")
+      table.Insert("Amber","Tester")
+      table.Insert("John","Tester")
+
+     table.Print()
+
+     //fmt.Print(table)
    }

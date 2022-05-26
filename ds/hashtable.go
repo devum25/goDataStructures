e38f03dddd7a2bc16981adb61dd1552d8be06712
dsrivast <devum.srivastava@gmail.com>
2022-05-26 13:03:25 +0530

    Hashtable implementation

---
+++ ds/hashtable.go
@@ -0,0 +1,82 @@
+package ds
+
+
+const ArraySize = 7
+
+//Hashtable structure
+// Hashtable will hold an array of type bucket
+type HashTable struct{
+	array [ArraySize]*bucket
+}
+
+func NewHashTable() *HashTable{
+        h := &HashTable{}
+		for i:=0;i<len(h.array);i++{
+			h.array[i] = &bucket{}
+		}
+		return h
+}
+
+//bucket structure
+// bucket is a linkedlist in each slot of the hashtable
+type bucket struct{
+	head *bucketNode
+}
+
+// bucketNode structure
+type bucketNode struct{
+	key string
+	val interface{}
+	next *bucketNode
+}
+//Insert
+func (h *HashTable) Insert(key string,val interface{}){
+	index := hash(key)
+	h.array[index].insert(key,val)
+}
+
+//Search
+func (h *HashTable) Search(key string) interface{}{
+	index := hash(key)
+	return h.array[index].search(key)
+	// return nil
+}
+
+//Delete
+func (h *HashTable) Delete(key string){
+	 index := hash(key)
+	 h.array[index].delete(key)
+}
+
+// bucket insert
+func (b *bucket) insert(k string,v interface{}){
+	newNode := &bucketNode{key: k,val: v}
+	newNode.next = b.head
+	b.head = newNode
+}
+// bucket search
+func (b *bucket) search(k string) interface{}{
+	currentNode := b.head
+	for currentNode != nil{
+		if currentNode.key == k{
+           return currentNode.val
+		}
+		currentNode = currentNode.next
+	}
+
+	return nil
+}
+// bucket delete
+func (b *bucket) delete(k string){
+
+}
+
+// hash function
+func hash(key string) int{
+	sum := 0
+	for _,v := range key{
+         sum += int(v)
+	}
+
+	return (sum%ArraySize)
+}
--- main.go
+++ main.go
@@ -7,57 +7,8 @@ import (
 )
 
 func main(){
-   // // l := ds.New()
-   // // l.Insert(5)
-   // // l.Insert(9)
-   // // l.Insert(10)
-   // // l.Insert(7)
-   // // l.Insert(80)
-   // // l.InsertAt(3,11)
+      table := ds.NewHashTable()
 
-   // // l.Print()
-   // // fmt.Println("Post delete")
-
-   // // l.DeleteAt(4)
-   
-   // // l.Print()
-   // l := New()
-   // l.Insert(1)
-   // l.Insert(2)
-   // l.Insert(3)
-   // l.Insert(4)
-
-   // // l.DeleteAt(2)
-   // // l.Search(2)
-   // l.GetAt(1)
-
-    heap := ds.NewHeap("Max")
-
-   heap.Array = append(heap.Array,100,40,50,20,30,15)
-
-   heap.Insert(120)
-
-   fmt.Println(heap.Array)
-
-   fmt.Println(heap.Pop())
-
-   fmt.Println(heap.Array)
-
-
-   // heap := ds.NewMinHeap()
-
-   //  heap.Array = append(heap.Array, 10,30,40,50,55,65,60)
-   
-   //  heap.Insert(5)
-
-   //  fmt.Println(heap.Array)
-
-   for i:=0;i<5;i++{
-
-   
-   fmt.Println(heap.Pop())
-
-   fmt.Println(heap.Array)
+      table.Insert("Devum","Developer")
+      fmt.Println(table.Search("Devum"))
    }
-   }
-   
